d561f9a6934b557c530e044c33b8e56fc7889abf
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-02-23 06:45:19 +0700

    - align new TileStore extensions

---
+++ MapboxMapObjC/Offline/TMBTileStoreObserver.swift
@@ -0,0 +1,8 @@
+//
+//  TMBTileStoreObserver.swift
+//  MapboxMapObjC
+//
+//  Created by Tuyen Vu on 2/23/24.
+//
+
+import Foundation
---
+++ MapboxMapObjC/Offline/TileStoreExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  TileStoreExtensions.swift
+//  MapboxMapObjC
+//
+//  Created by Tuyen Vu on 2/23/24.
+//
+
+import Foundation
